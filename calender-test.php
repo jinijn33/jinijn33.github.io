02ebb7cd7e2fc2aba495642ad0ff0836f2928e78
jinijn33 <minseck68@gmail.com>
2025-04-09 10:37:32 +0900

    calender example

---
+++ calender-test.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <style>
+    font.holy {font-family: tahoma;font-size: 20px;color: #FF6C21;}
+    font.blue {font-family: tahoma;font-size: 20px;color: #0000FF;}
+    font.black {font-family: tahoma;font-size: 20px;color: #000000;}
+  </style>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<table class="table table-bordered table-responsive">
+  <tr align="center" >
+    <td>
+        <a href=test2.html?year=2024&month=4&day=1>◀◀</a>
+    </td>
+    <td>
+        <a href=test2.html?year=2025&month=3&day=1>◀</a>
+    </td>
+    <td height="50" bgcolor="#FFFFFF" colspan="3">
+        <a href=test2.html?year=2025&month=4&day=1>
+        &nbsp;&nbsp;2025년 4월 &nbsp;&nbsp;</a>
+    </td>
+    <td>
+        <a href=test2.html?year=2025&month=5&day=1>▶</a>
+    </td>
+    <td>
+        <a href=test2.html?year=2026&month=4&day=1>▶▶</a>
+    </td>
+  </tr>
+  <tr class="info">
+    <th hight="30">일</td>
+    <th>월</th>
+    <th>화</th>
+    <th>수</th>
+    <th>목</th>
+    <th>금</th>
+    <th>토</th>
+  </tr>
+
+    <tr>
+    <td height="50" valign="top"></td><td height="50" valign="top"></td><td height="50" valign="top"><font class=black>1</font></td><td height="50" valign="top"><font class=black>2</font></td><td height="50" valign="top"><font class=black>3</font></td><td height="50" valign="top"><font class=black>4</font></td><td height="50" valign="top"><font class=blue>5</font></td>  </tr>
+    <tr>
+    <td height="50" valign="top"><font class=holy>6</font></td><td height="50" valign="top"><font class=black>7</font></td><td height="50" valign="top"><font class=black>8</font></td><td height="50" valign="top"><font class=black>9</font></td><td height="50" valign="top"><font class=black>10</font></td><td height="50" valign="top"><font class=black>11</font></td><td height="50" valign="top"><font class=blue>12</font></td>  </tr>
+    <tr>
+    <td height="50" valign="top"><font class=holy>13</font></td><td height="50" valign="top"><font class=black>14</font></td><td height="50" valign="top"><font class=black>15</font></td><td height="50" valign="top"><font class=black>16</font></td><td height="50" valign="top"><font class=black>17</font></td><td height="50" valign="top"><font class=black>18</font></td><td height="50" valign="top"><font class=blue>19</font></td>  </tr>
+    <tr>
+    <td height="50" valign="top"><font class=holy>20</font></td><td height="50" valign="top"><font class=black>21</font></td><td height="50" valign="top"><font class=black>22</font></td><td height="50" valign="top"><font class=black>23</font></td><td height="50" valign="top"><font class=black>24</font></td><td height="50" valign="top"><font class=black>25</font></td><td height="50" valign="top"><font class=blue>26</font></td>  </tr>
+    <tr>
+    <td height="50" valign="top"><font class=holy>27</font></td><td height="50" valign="top"><font class=black>28</font></td><td height="50" valign="top"><font class=black>29</font></td><td height="50" valign="top"><font class=black>30</font></td><td height="50" valign="top"></td><td height="50" valign="top"></td><td height="50" valign="top"></td>  </tr>
+  </table>
+</div>
+
+</body>
+</html>
+출처: https://cnisoft.tistory.com/237 [씨엔아이스토리:티스토리]
